3020089d171c749bff634dcd8b4da08926da86a3
wizard <ivb9891@yandex.ru>
2021-05-18 16:39:55 +0300

    Ev2 - gonfig add db configs

--- domain/EposConfig.go
+++ domain/EposConfig.go
@@ -1,10 +1,11 @@
 package domain
 
 type EposConfig struct {
-	PathToBuffer string
-	ParseLimit   int
-	AuthHeader   string
-	SaveToDb     bool
-	GraphicsPath string
-	GraphVpnSave bool
+	PathToBuffer  string
+	ParseLimit    int
+	AuthHeader    string
+	DbMinerPath   string
+	DbGeneralPath string
+	GraphicsPath  string
+	GraphVpnSave  bool
 }
--- utils/getConfig_test.go
+++ utils/getConfig_test.go
@@ -6,7 +6,8 @@ func Test_GetConfig(t *testing.T) {
 	conf := GetConfig("../config/test-conf.json")
 
 	if conf.ParseLimit != 50 || conf.GraphVpnSave != false || conf.PathToBuffer != "/testPath" ||
-		conf.SaveToDb != true || conf.GraphicsPath != "/testPath" || conf.AuthHeader != "test-header" {
+		conf.DbMinerPath != "/testDbPath" || conf.GraphicsPath != "/testPath" || conf.AuthHeader != "test-header" ||
+		conf.DbGeneralPath != "/testDbPath" {
 		t.Fail()
 	}
 }
